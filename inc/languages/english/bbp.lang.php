3010767249d215ef5e9be3f984155f716c11929d
Brian <31559308+BrianLeek@users.noreply.github.com>
2018-05-18 14:59:55 -0600

    Created bbp.lang.php

---
+++ inc/languages/english/bbp.lang.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin: Block Banned Profiles
+ * Description: Blocks users access to the profiles of banned users.
+ * File: bbp.lang.php
+ * Author:  Brian. ( https://community.mybb.com/user-115119.html )
+ * Version 1.1
+**/
+
+$l['bbp_statement'] = "<center>There is no available information for <b>{1}</b>
+<br />
+<br />
+Account has been Suspended</center>";
+
+?>
